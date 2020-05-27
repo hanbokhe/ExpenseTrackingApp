94153b9d58d33ee288958d71ad0e5d5f286a0aab
Soo Kim <hangbokhe@users.noreply.github.com>
2020-05-27 01:17:11 -0700

    Added Classes

---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Model/Budget.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTrackingApp.Model
+{
+
+    public class Budget
+    {
+        public decimal TotalBudget { get; set; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var transaction in allTransactions)
+                {
+                    balance += transaction.Amount;
+                    
+                }
+                return balance;
+            } 
+        }
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public Budget()
+        {
+            //Ana, I need help~
+        }
+
+        public void Spend(decimal amount, DateTime date, MonthBudget month, string name)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Enter a positive number");
+            }
+            var spent = new Transaction(amount, date, month, name);
+            allTransactions.Add(spent);
+        }
+        public void Save(decimal amount, DateTime date, MonthBudget month, string name)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Enter a positive number");
+            }
+
+            var save = new Transaction(-amount, date, month, name);
+            allTransactions.Add(save);
+
+            //alert for exceeding budget
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Budget Exceeded");
+            }
+
+        }
+        //public string GetTransaction History()
+        //{
+        //    var report = new System.Text.StringBuilder();
+        //    decimal balance = 0;
+        //}
+    }
+}
---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Model/Transaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTrackingApp.Model
+{
+    public enum TransactionType
+    {
+        Car,
+        Entertainment,
+        Food,
+        Gas,
+        Shopping,
+        Rent,
+    }
+    public enum MonthBudget
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December,
+    }
+    class Transaction
+    {
+        public string Name { get; set; }
+        public MonthBudget Month { get; }
+        public DateTime Date { get; }
+        public decimal Amount { get; }
+        public Transaction(decimal amount, DateTime date, MonthBudget month, string name)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Name = name;
+            this.Month = month;
+        }
+
+    }
+
+}
