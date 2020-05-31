4952152965ec3ed0499d111131c5aaf044a56739
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-05-31 16:02:56 -0700

    trying grouping on transaction page

---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Model/TransactionList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTrackingApp.Model
+{
+    class TransactionList
+    {
+        public string Category { get; set; }
+        public double SpendAmount { get; set; }
+
+        public List<Transaction> TransactionsByCategory { get; set; }
+    }
+}
